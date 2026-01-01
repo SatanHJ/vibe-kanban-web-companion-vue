bca7b491dcb978ea5fd831b39c85ecdb736d68be
heji <wisdomsatan@gmail.com>
2026-01-01 11:13:45 +0800

    fix

--- packages/vibe-kanban-web-companion/src/index.js
+++
@@ -1,5 +0,0 @@
-import { default as Component } from './VibeKanbanWebCompanion.vue'
-import { h } from 'vue'
-
-export const VibeKanbanWebCompanion =
-  process.env.NODE_ENV === 'development' ? Component : { render: () => h('div') }
